c92b58a2faf0caebc4e0a21b9e26de9a8b01772e
Natã Araujo <natan.mca@gmail.com>
2022-09-27 16:17:19 -0300

    Update card expense

--- src/components/CardExpense/index.tsx
+++ src/components/CardExpense/index.tsx
@@ -1,16 +1,40 @@
-import { Box } from '@mui/material'
-import TableExpense from 'components/TableExpense'
-import * as S from './styles'
+import * as S from './styles';
+import { Box, Typography } from '@mui/material';
+import TableExpense from 'components/TableExpense';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 
-type Props = {}
+type Props = {};
 
 export default function CardExpense({}: Props) {
   return (
-    <S.Whapper >
+    <S.Whapper>
       <S.Card elevation={3}>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          gap="8px"
+          height="50px"
+          bgcolor="error.main"
+        >
+          <Typography variant="h5">Despesas</Typography>
+          <ArrowDownwardIcon />
+        </Box>
+
         <TableExpense />
+        
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          gap="8px"
+          height="100px"
+          bgcolor="background.grey"
+        >
+          <Typography variant="h5">Total:</Typography>
+          <Typography variant="h5">R$ 0,00</Typography>
+        </Box>
       </S.Card>
-      <S.Card elevation={3}></S.Card>
     </S.Whapper>
-  )
+  );
 }
--- src/components/TableExpense/styles.ts
+++ src/components/TableExpense/styles.ts
@@ -3,16 +3,30 @@ import { Box } from "@mui/material";
 import theme from "theme/";
 
 export const Whapper = styled(Box) `
-  overflow: scroll;
-  border-radius: 20px;
-  height: 400px;
+  max-height: 400px;
+  overflow: auto;
   ::-webkit-scrollbar {
-    width: 0px;
-}
+    width:10px
+  }   
+  ::-webkit-scrollbar {
+    width:10px
+  }   
+  ::-webkit-scrollbar-track {
+    background-color: ${theme.palette.grey[500]};
+  }   
+  ::-webkit-scrollbar-thumb {
+    background-color: ${theme.palette.grey[700]};
+  }      
+  ::-webkit-scrollbar-corner {
+    background-color: #999;
+  }   
   .MuiTableRow-root {
     height: 50px;
   }
   .MuiTableRow-root:nth-child(even) {
     background-color: ${theme.palette.grey[800]}
+  }
+  .MuiTableContainer-root {
+    overflow-x: none;
   }
 `
